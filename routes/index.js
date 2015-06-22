a8bc00be7dedb6446c0aae8bec3569f69744d079
JorgeBailo <github@jorgebailo.es>
2015-06-22 10:04:54 +0200

    cambio jade - ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz', description: 'El portal donde podrá crear sus propios juegos' });
 });
 
 module.exports = router;
